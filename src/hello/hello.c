820b8b9cdcd94b8cdea276e7a2c20af6eeca3c8d
newcliff <neocliff@mac.com>
2020-01-01 13:12:31 -0600

    a few more changes to get the build to work

--- src/hello/hello.c
+++ src/hello/hello.c
@@ -5,10 +5,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "hello.h"
+
 int main (int, char **, char **);
 
 int main (int argc, char *argv[], char *envp[]) {
-    print("hello, world!");
+    print("hello, world!\n");
+    print("built for BITSIZE: %d\n", BITSIZE)
 
     return(0);
 }
---
+++ src/hello/include/hello.h
@@ -0,0 +1,10 @@
+/**
+ * toy header file defining a constant.
+ */
+
+#ifndef _hello_h
+#define _hello_h
+
+#define BITSIZE 64
+
+#endif /* _hello_h */
