fb1328d6eafcbc0a9ab7a921682e7b3f1d9ddeed
newcliff <neocliff@mac.com>
2020-01-01 12:54:01 -0600

    add compiler_rules.mk, component.mk, and hello.c

---
+++ src/hello.c
@@ -0,0 +1,14 @@
+/**
+ * hello.c - typical "hello world!" code enhanced for this project
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int, char **, char **);
+
+int main (int argc, char *argv[], char *envp[]) {
+    print("hello, world!");
+
+    return(0);
+}
---
+++ src/hello/component.mk
@@ -0,0 +1,8 @@
+# component.mk - component's description file
+
+BINBUILT = hello
+
+INCDIR = include
+INCLUDES =
+
+SRCS := hello.c
