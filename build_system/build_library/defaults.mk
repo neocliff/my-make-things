2af392a4f0bc1cfeeb848c53a7710921dc936d44
newcliff <neocliff@mac.com>
2020-01-01 14:22:42 -0600

    move several default dirs from makefile to defaults.mk

--- build_system/build_library/defaults.mk
+++ build_system/build_library/defaults.mk
@@ -10,3 +10,20 @@ CXX		?= ${USR_BINDIR}/g++
 LD		?= ${USR_BINDIR}/gold
 TAR		?= ${SYS_BINDIR}/tar
 AR		?= ${USR_BINDIR}/ar
+
+# define the default path for header files. unlike this such
+# as the directory for obj files, the INC_DIR is a list of
+# directories that is scanned to locate header files. that
+# means we could have multiple paths.
+INC_DIR ?= ${I_AM_AT}/include
+
+# define the default paths for everything else. these can be
+# redefined in the component.mk files using something like the
+# following syntax:
+#		OBJDIR = my/obj/dir/path
+DEP_ROOT	?= ${I_AM_AT}/dep
+DEP_DIR		?= $(DEP_ROOT)/$(BUILD_TYPE)/$(ARCH_TYPE)
+OBJ_ROOT	?= ${I_AM_AT}/obj
+OBJ_DIR		?= $(OBJ_ROOT)/$(BUILD_TYPE)/$(ARCH_TYPE)
+BIN_ROOT	?= ${I_AM_AT}/bin
+BIN_DIR		?= $(BIN_ROOT)/$(BUILD_TYPE)/$(ARCH_TYPE)
