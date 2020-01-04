76082faeaafd6dba78858f8e28a1546b06a00031
newcliff <neocliff@mac.com>
2020-01-04 15:48:05 -0600

    more doxygen stuff

--- src/b_dir/b2.c
+++ src/b_dir/b2.c
@@ -1,3 +1,14 @@
+/**
+ * @file b2.c
+ * @brief an "internal" object file included in b_lib.ar
+ * @author Cliff Williams
+ * 
+ * @desc This file is an "internal" object file that is linked into
+ * the b_lib.arc archive. The archive is then linked to the hello 
+ * executable.
+ *
+ * @bug no known bugs 
+ */
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -6,6 +17,14 @@
 
 int b2();
 
+**
+ * b2() - returns a constant
+ * 
+ * @desc This is an "internal" function called from @c b1(). It just
+ * returns a constant.
+ * 
+ * @return the constant MY_NUMBER_2
+ */
 int b2() {
     printf("b2: returning %d\n", MY_NUMBER_2);
     return(MY_NUMBER_2);
