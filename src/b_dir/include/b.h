b25f1b3dacca141ba22a556c7e50c2fe0f493d3f
newcliff <neocliff@mac.com>
2020-01-20 13:12:58 -0600

    add variadic args macro

--- src/b_dir/include/b.h
+++ src/b_dir/include/b.h
@@ -16,6 +16,20 @@
 #define MY_NUMBER_1 100
 #define MY_NUMBER_2 200
 
+/**
+ * MY_VA_MACRO() is a macro that accepts variadic arguments and turns themxi
+ * into a printf() call.
+ * 
+ * In theory, the declaration below *should* support calls like
+ *     MY_VA_MACRO("hello, world!\n")
+ * It is a built-in behavior for GNU C.
+ * 
+ * @param s - control string
+ * @param va_args - the '...' part that can be whatever the caller wants
+ * @returns value returned by printf()
+ */
+#define MY_VA_MACRO(s, ...) printf(s, ##__VA_ARGS__ )
+
 extern int b1();
 extern int b2();
 
