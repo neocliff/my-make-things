dd43a4c3fa44a2da72ab3fa35167e9d8cb107090
newcliff <neocliff@mac.com>
2020-03-17 15:40:53 -0500

    added system_header pragma

--- src/hello/c_dir/include/c.h
+++ src/hello/c_dir/include/c.h
@@ -13,7 +13,6 @@
 
 #include <stdio.h>
 
-//#pragma GCC system_header
 /**
  * We are expiramenting with variadic argments in macros. The project at
  * the office uses them extensively. The problem is that they don't always
@@ -24,7 +23,14 @@
  * 
  * Compile with '-std=c11' and get a warning;
  * Compile with '-std=gnu11' and get a warning
+ *
+ * The only way to avoid the warnings (for now) appears to be the nuclear option:
+ * the 'system_header' pragma. Be warned though, using this makes GCC think this
+ * is a system header file. It cause the compiler to dispense with most, maybe
+ * all, of the warnings it would generate based on the command-line options. If
+ * you have to use it, make the code as small as possible.
  */
+#pragma GCC system_header
 #define MY_PRINT(s, ...) printf(s, ##__VA_ARGS__)
 
 extern char *c();
