3c2f89f9d7997837443734d58a603c85124a9b61
newcliff <neocliff@mac.com>
2020-03-19 09:37:06 -0500

    Doxygen comment updates

--- src/shared/s_dir_191230/include/s.h
+++ src/shared/s_dir_191230/include/s.h
@@ -1,6 +1,11 @@
 #ifndef _s_h
 #define _s_h
 
+/**
+ * @brief s() returns a canned integer to the caller.
+ * 
+ * @return int 
+ */
 extern int s();
 
 #endif
--- src/shared/s_dir_191230/s.c
+++ src/shared/s_dir_191230/s.c
@@ -1,4 +1,13 @@
-
+/**
+ * @file s.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-03-09
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
 
 #include <stdlib.h>
 #include <stdio.h>
