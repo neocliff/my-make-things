dd1431bca1a4e3528f5232b096c02974ba317921
newcliff <neocliff@mac.com>
2020-03-20 16:57:35 -0500

    update docker run line

--- build_system/build_utilities/run_docker_image.sh
+++ build_system/build_utilities/run_docker_image.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR=$(dirname "$0")
 pushd "$SCRIPT_DIR" > /dev/null
 BASE_DIR=$(readlink -f "${SCRIPT_DIR}")
 
-docker run --rm -it -v $BASE_DIR:/home/user/my-make-things user:dev bash my-make-things/build.sh
+docker run --rm -it -v $BASE_DIR:/home/user/Documents/my-make-things user:dev bash my-make-things/build.sh
