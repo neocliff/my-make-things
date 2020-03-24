84a888f174ca4d8c53743827195ccea8e085d86b
newcliff <neocliff@mac.com>
2020-03-24 12:16:50 -0500

    tweak the docker run command

--- build_system/build_utilities/run_docker_image.sh
+++ build_system/build_utilities/run_docker_image.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(dirname "$0")
-pushd "$SCRIPT_DIR" > /dev/null
-BASE_DIR=$(readlink -f "${SCRIPT_DIR}")
+#SCRIPT_DIR=$(dirname "$0")
+#pushd "$SCRIPT_DIR" > /dev/null
+#BASE_DIR=$(readlink -f "${SCRIPT_DIR}")
 
-docker run --rm -it -v $BASE_DIR:/home/user/Documents/my-make-things user:dev bash my-make-things/build.sh
+#docker run --rm -it -v $BASE_DIR:/home/user/Documents/my-make-things toolset:latest bash my-make-things/build.sh
+
+# format is:
+#   docker run 
+#       --rm        automatically delete the container when it exits
+#       -i          run the container in interactive mode
+#       -t          allocate a pseudo-tty for the console
+#       -v          connect a volume using "source:destination" syntax
+#           `git rev-parse --show-toplevel` is the root of the current repo (source)
+#           /home/user/my-make-things is the mount point in the container (destination)
+#       <image>     name of the container image in "image:tag" format
+#       <cmd [opts]>
+#                   name of the command to run in the container, usually a shell
+#                   with optional parameters
+docker run --rm -it -v `git rev-parse --show-toplevel`:/home/user/my-make-things toolset:latest /bin/bash
