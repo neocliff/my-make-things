123423b9627466f193ed44d6e854437be02a5e82
Cliff Williams <neocliff@mac.com>
2020-04-09 14:15:01 -0500

    initial checkin

--- build_system/build_utilities/run_docker_interactive.sh
+++ build_system/build_utilities/run_docker_interactive.sh
@@ -15,16 +15,7 @@ fi
 echo "launching interactive toolchain container..."
 
 # set the in-container working directory to be the current working directory.
-# at first, i used the following line of commands to set the value and then
-# it hit me that i could just make WORKING_dir the same at the PWD because
-# it is going to be that in the container. duh!
-#working_dir=$(echo `pwd` | sed "s,`git rev-parse --show-toplevel`/,,")
 WORKING_DIR=`pwd`
-
-# everything else in the arguments from the cli is to be passed into the container
-# as the command to be run. this code will work because there is at least one
-# argument left or we wouldn't have gotten this far.
-#command_to_run="$(printf "${1+ %q}" "$@")"
 
 # format of 'docker run' command is:
 #   docker run
